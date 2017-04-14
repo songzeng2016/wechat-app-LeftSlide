4ab71f15b049c42a9af3cb840fd32330f2621af9
Song Zeng <1561192122@qq.com>
2017-04-14 17:05:45 +0800

    init project

---
+++ pages/leftSlide/leftSlide.js
@@ -0,0 +1,43 @@
+// pages/leftSlide/leftSlide.js
+
+const App = getApp()
+import itemData from './mock.js'
+
+Page({
+  data: {
+    itemData,
+  },
+  touchS: function (e) {  // touchstart
+    let startX = App.Touches.getClientX(e)
+    startX && this.setData({ startX })
+  },
+  touchM: function (e) {  // touchmove
+    let itemData = App.Touches.touchM(e, this.data.itemData, this.data.startX)
+    itemData && this.setData({ itemData })
+
+  },
+  touchE: function (e) {  // touchend
+    const width = 150  // 定义操作列表宽度
+    let itemData = App.Touches.touchE(e, this.data.itemData, this.data.startX, width)
+    itemData && this.setData({ itemData })
+  },
+  itemDelete: function(e){  // itemDelete
+    let itemData = App.Touches.deleteItem(e, this.data.itemData)
+    itemData && this.setData({ itemData })
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
---
+++ pages/leftSlide/mock.js
@@ -0,0 +1,88 @@
+const itemData = [
+    {
+        no: '001',
+        img: '/images/img.png',
+        name: '宋双庙',
+        info: '我：二狗，你妈喊你回家吃饭',
+        time: '上午12:00',
+    },
+    {
+        no: '001',
+        img: '/images/mp3.png',
+        name: '订阅号',
+        info: '拜拜吧比较爱白芭比白阿比',
+        time: '昨天',
+    },
+    {
+        no: '001',
+        img: '/images/pdf.png',
+        name: '微信团队',
+        info: '登录操作通知',
+        time: '上午 9:00',
+    },
+    {
+        no: '001',
+        img: '/images/txt.png',
+        name: '锤子科技',
+        info: '罗永浩 x 罗振宇访谈节目 《长谈》',
+        time: '星期二',
+    },
+    {
+        no: '001',
+        img: '/images/word.png',
+        name: '微信公众平台安全助手',
+        info: '小程序登录提醒',
+        time: '上午10:00',
+    },
+    {
+        no: '001',
+        img: '/images/excel.png',
+        name: '微信支付',
+        info: '支付成功',
+        time: '上午12:00',
+    },
+    {
+        no: '001',
+        img: '/images/img.png',
+        name: '宋双庙',
+        info: '我：二狗，你妈喊你回家吃饭',
+        time: '上午12:00',
+    },
+    {
+        no: '001',
+        img: '/images/mp3.png',
+        name: '订阅号',
+        info: '拜拜吧比较爱白芭比白阿比',
+        time: '昨天',
+    },
+    {
+        no: '001',
+        img: '/images/pdf.png',
+        name: '微信团队',
+        info: '登录操作通知',
+        time: '上午 9:00',
+    },
+    {
+        no: '001',
+        img: '/images/txt.png',
+        name: '锤子科技',
+        info: '罗永浩 x 罗振宇访谈节目 《长谈》',
+        time: '上午12:00',
+    },
+    {
+        no: '001',
+        img: '/images/word.png',
+        name: '微信公众平台安全助手',
+        info: '小程序登录提醒',
+        time: '上午10:00',
+    },
+    {
+        no: '001',
+        img: '/images/excel.png',
+        name: '微信支付',
+        info: '支付成功',
+        time: '上午12:00',
+    },
+]
+
+export default itemData
